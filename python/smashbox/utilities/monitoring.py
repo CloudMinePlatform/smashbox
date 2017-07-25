eefd47492afdb08c7d3e3ea22a66afc48a9079c9
yolanda93 <yolanda93h@gmail.com>
2017-07-25 14:52:56 +0200

    Updated report schema

--- python/smashbox/utilities/monitoring.py
+++ python/smashbox/utilities/monitoring.py
@@ -6,13 +6,14 @@ import sys
 import json
 import urllib2
 
-
+#--keep-going -a --loop=40 --quiet C:\Users\ydelahoz\workspace\smashbox\lib\
 class HeadRequest(urllib2.Request):
       def get_method(self):
         return "HEAD"
 
 class StateMonitor:
-    # {'subtest_id':subtest_id , 'parameters': params, 'errors': [], 'success': [], 'operations':[], 'qos_metrics': []}
+    # Test information  -->  {'test_name': string, 'timestamp': string, 'hostname': string, 'oc_client version': string, 'eos_version': string, 'platform': string, 'passed': int, 'failed': int, 'subtests': [self.test_results]}
+    # Subtests information --> {'subtest_id':subtest_id , 'parameters': params, 'errors': [], 'success': [], 'operations':[], 'qos_metrics': [], 'total_errors':0, 'total_success':0}
 
     def __init__(self,manager):
         self.testname = None
@@ -30,13 +31,15 @@ class StateMonitor:
         self.nsubtests =  config.__dict__["nsubtests"]
         self.smashdir = config.smashdir
 
-        params = []
+        self.parameters = []
+        param = {}
         for c in config.__dict__:
             if c.startswith(self.testname + "_"):
-                params.append(str((c.replace(self.testname + "_", ""), config[c])))
+                param[str((c.replace(self.testname + "_", "")))] = config[c]
+                self.parameters.append(param)
                 print c, config[c]
 
-        self.test_results = {'subtest_id':self.subtest_id,'parameters':params,'errors': [], 'success': [], 'operations':[], 'qos_metrics': []}
+        self.test_results = {'subtest_id':self.subtest_id,'parameters':[],'errors': [], 'success': [], 'operations':[], 'qos_metrics': [], 'total_errors':0,'total_success':0}
 
 
     def join_worker_results(self):
@@ -44,38 +47,51 @@ class StateMonitor:
         Join partial worker tests results information. The partial results are stored in queue (FIFO order)
         """
         partial_results = self.worker_results.get()
-        self.test_results['errors'].append(partial_results[0])
-        self.test_results['success'].append(partial_results[1])
+        if(partial_results[0]): self.test_results['errors'].append(partial_results[0])
+        if(partial_results[1]): self.test_results['success'].append(partial_results[1])
         self.test_results['operations'].append(partial_results[2])
-        self.test_results['qos_metrics'].append(partial_results[3])
 
+        if(len(partial_results[3])!=0): # normally only one worker has the qos_metrics
+            self.test_results['qos_metrics'].append(partial_results[3][0])
+
+        self.test_results['total_errors']+=len(self.test_results['errors'])
+        self.test_results['total_success']+=len(self.test_results['success'])
+
+        if (len( self.parameters) > self.subtest_id and len(self.test_results['parameters'])==0): # add subtests parameters
+            self.test_results['parameters'].append(self.parameters[self.subtest_id])
 
     def subtestcase_finish(self):
         """
-        Update the dictionary with the last results and publish
+        Update the dictionary with the last results and publish at the end
         """
         json_results = self.get_json_results()
+
         if(self.subtest_id==0): # first iteration
-            json_result = json.dumps(json_results)
             if (self.subtest_id>=self.nsubtests-1): # no subtests
-                self.test_finish(json_result)
-            self.save_json(json_result)
+                self.test_finish(json_results)
+            self.save_json(json_results)
         else:
             data = self.get_json()
             data['subtests'].append(self.test_results)
-            json_results = json.dumps(data)
+
             if(self.subtest_id>=self.nsubtests-1):
-                self.test_finish(json_results)
+                self.test_finish(data)
             else:
-                self.save_json(json_results)
+                self.save_json(data)
 
     def save_json(self,data):
-        dir = self.smashdir + "/" + "json_results" + "/" + str(self.testname)
+        """
+        Save json results in a file to preserve the state between main program calls 
+        """
+        data = json.dumps(data)
+        time = str(self.runid).split(" ")
+        dir = self.smashdir + "/" + "json_results" + "/" + str(self.testname) + "/" + time[0]
 
         if not os.path.exists(dir):
             os.makedirs(dir)
 
-        filename =  dir   + "/" +  str(self.runid) + ".txt"
+        filename = dir + "/" +  time[1].replace(":","-") + ".txt"
+
         try:
             fd = open(filename, 'w')
             fd.write(data)
@@ -85,8 +101,13 @@ class StateMonitor:
             pass
 
     def get_json(self):
-        dir = self.smashdir + "/" + "json_results" + "/" + str(self.testname)
-        filename = dir + "/" + str(self.runid) + ".txt"
+        """
+        Restore json results previously saved in the file 
+        """
+        time = str(self.runid).split(" ")
+        dir = self.smashdir + "/" + "json_results" + "/" + str(self.testname) + "/" + time[0]
+
+        filename = dir + "/" +  time[1].replace(":","-") + ".txt"
 
         returndata = {}
         try:
@@ -99,22 +120,37 @@ class StateMonitor:
         return returndata
 
     def test_finish(self,json_results):
-        print self.publish_results(json_results)
+        """
+        Compute total passed subtests and publish results
+        """
+        # compute passed subtests
+        failed = 0
+        for subtest in json_results['subtests']:
+            if(subtest['total_errors']>=1): # A subtest is considered failed with one or more errors
+                failed += 1
+
+        json_results['failed']= failed
+        json_results['passed']=len(json_results['subtests']) - failed
+
+        print self.publish_results(json.dumps(json_results))
 
     def get_json_results(self):
-        json_result = {'test_name': self.testname, 'timestamp': self.runid, 'hostname': socket.gethostname(),
-                                  'oc_client version': str(ocsync_version()), 'eos_version': "beryl_aquamarine",
+        """
+        Saved results in a dictionary to be able to convert them in a json format
+        """
+        json_result = {'test_name': self.testname, 'timestamp': self.runid.replace(" ","T")+"Z", 'hostname': socket.gethostname(),
+                                  'oc_client version': str(str(ocsync_version())[1:-1].replace(",",".")), 'eos_version': "beryl_aquamarine",
                                   'platform': platform.system() + platform.release(), 'subtests': [self.test_results]}
         return json_result
 
 
     def publish_results(self,data):
         """
-        Update the dictionary with the last results and publish
+        Publish results in a kibana dashboard
         """
         url_parameters = {'cluster': "http://smashbox-monitoring:9200/smashbox-tests",
                           'index': self.testname,
-                          'index_period':self.runid, }
+                          'index_period':self.runid.replace(" ","T")+"Z", }
         url = "%(cluster)s/%(index)s/%(index)s-%(index_period)s" % url_parameters
         url = url
         headers = {'content-type': 'application/json'}
@@ -125,31 +161,6 @@ class StateMonitor:
         except Exception as e:
             response = "Error:  {}".format(str(e))
         return response
-
-    #
-    # def publish_results(self, data):
-    #     url_parameters = {'cluster': "http://smashbox-monitoring:9200",
-    #                       'index': self.testname,
-    #                       'index_period':self.runid, }
-    #     type = "/message"
-    #     url = "%(cluster)s/%(index)s/%(index)s-%(index_period)s" % url_parameters
-    #
-    #     if(self.exists_test(url + type)):# check if there is an entry with the test run id
-    #         print("The index already exitsts in the database")
-    #         data = {'script': {'lang': "painless", 'inline': "ctx._source.subtests.add(params.subtest)", 'params' : {'subtest' : "sadas" }}}
-    #         print self.push_to_monitoring(json.dumps(data), url + type + "/" + "_update" )
-    #     else: # create a new index for this runid
-    #         print self.push_to_monitoring(data, url + type)
-    #
-    # def exists_test(self, url):
-    #     try:
-    #         response = urllib2.urlopen(HeadRequest(url)).geturl()
-    #     except Exception as e:
-    #         response = "Error:  {}".format(str(e))
-    #     return response == url
-
-
-
 
 
 
